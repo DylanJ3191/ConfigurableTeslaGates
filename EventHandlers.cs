3a157b2dcb13484155507743015e5a204ac94ab9
DylanJ3191 <111197787+DylanJ3191@users.noreply.github.com>
2025-12-05 19:33:55 -0600

    Initial commit

---
+++ EventHandlers.cs
@@ -0,0 +1,58 @@
+﻿using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.CustomHandlers;
+
+namespace ConfigurableTeslaGates;
+
+public class EventHandlers : CustomEventsHandler
+{
+    public override void OnPlayerTriggeringTesla(PlayerTriggeringTeslaEventArgs args)
+    {
+        if (Plugin.Main.Config is null)
+            return;
+
+        if (Plugin.Main.Config.GatesEnabled == false)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Tutorial && Plugin.Main.Config.Tutorial == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.ClassD && Plugin.Main.Config.ClassD == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Scientist && Plugin.Main.Config.Scientist == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Scp049 && Plugin.Main.Config.Scp049 == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Scp0492 && Plugin.Main.Config.Scp0492 == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Scp096 && Plugin.Main.Config.Scp096 == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Scp106 && Plugin.Main.Config.Scp106 == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Scp173 && Plugin.Main.Config.Scp173 == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Scp939 && Plugin.Main.Config.Scp939 == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.Scp3114 && Plugin.Main.Config.Scp3114 == true)
+            args.IsAllowed = false;
+
+        if ((args.Player.Role == PlayerRoles.RoleTypeId.Flamingo || args.Player.Role == PlayerRoles.RoleTypeId.AlphaFlamingo || args.Player.Role == PlayerRoles.RoleTypeId.ZombieFlamingo) && Plugin.Main.Config.Flamingos == true)
+            args.IsAllowed = false;
+
+        if ((args.Player.Role == PlayerRoles.RoleTypeId.ChaosConscript || args.Player.Role == PlayerRoles.RoleTypeId.ChaosMarauder || args.Player.Role == PlayerRoles.RoleTypeId.ChaosRepressor || args.Player.Role == PlayerRoles.RoleTypeId.ChaosRifleman) && Plugin.Main.Config.ChaosInsurgency == true)
+            args.IsAllowed = false;
+
+        if ((args.Player.Role == PlayerRoles.RoleTypeId.NtfPrivate || args.Player.Role == PlayerRoles.RoleTypeId.NtfSpecialist || args.Player.Role == PlayerRoles.RoleTypeId.NtfSergeant || args.Player.Role == PlayerRoles.RoleTypeId.NtfCaptain) && Plugin.Main.Config.MobileTaskForces == true)
+            args.IsAllowed = false;
+
+        if (args.Player.Role == PlayerRoles.RoleTypeId.FacilityGuard && Plugin.Main.Config.FacilityGuard == true)
+            args.IsAllowed = false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+namespace ConfigurableTeslaGates;
+
+using System;
+using LabApi.Events.CustomHandlers;
+using LabApi.Features;
+using LabApi.Loader.Features.Plugins;
+using LabApi.Loader.Features.Plugins.Enums;
+
+public class Plugin : Plugin<Config>
+{
+	public static Plugin Main { get; set; } = null;
+
+	public override string Name { get; } = "Configurable Tesla Gates";
+
+	public override string Author { get; } = "DylanJ3191";
+
+	public override string Description { get; } = "Config options for Tesla Gates";
+
+	public override Version Version { get; } = new Version(1,0,1);
+
+	public override Version RequiredApiVersion { get; } = new Version(LabApiProperties.CompiledVersion);
+
+	public override LoadPriority Priority { get; } = LoadPriority.High;
+
+	public EventHandlers Events { get; } = new();
+
+	public override void Enable()
+	{
+		Main = this;
+        CustomHandlersManager.RegisterEventsHandler(Events);
+		if (Config.GatesEnabled == false)
+			TeslaGateController.Singleton.enabled = false;
+    }
+
+    public override void Disable()
+	{
+		Main = null;
+		CustomHandlersManager.UnregisterEventsHandler(Events);
+	}
+}
